bf11affe0968cfcb247834e67eda9fdaeb8dcb1e
Nil20 <nileeeem36@gmail.com>
2022-12-28 19:50:42 +0600

    InformantSMSNotification model addition

---
+++ packages/config/src/models/informantSMSNotification.ts
@@ -0,0 +1,32 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+
+import { Document, model, Schema } from 'mongoose'
+
+export interface IInformantSMSNotificationsModel extends Document {
+  name: string
+  enabled: boolean
+  createdAt: number
+  updatedAt: number
+}
+
+const informantSMSNotificationsSchema = new Schema({
+  name: { type: String, unique: true, required: true },
+  enabled: { type: Boolean, required: true, default: false },
+  createdAt: { type: Number, default: Date.now(), required: true },
+  updatedAt: { type: Number, default: Date.now(), required: true }
+})
+
+export default model<IInformantSMSNotificationsModel>(
+  'InformantSMSNotification',
+  informantSMSNotificationsSchema
+)
