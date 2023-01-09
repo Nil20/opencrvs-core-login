6bad40160b1c317953349ad9c2dc01d84dc03d97
Nil20 <nileeeem36@gmail.com>
2023-01-09 14:23:14 +0600

    store.ts inspect in feature/login-background

--- packages/client/src/views/SysAdmin/Config/Application/Tabs/GeneralProperties/LoginBackground.tsx
+++ packages/client/src/views/SysAdmin/Config/Application/Tabs/GeneralProperties/LoginBackground.tsx
@@ -9,3 +9,37 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+
+import { getOfflineData } from '@client/offline/selectors'
+import { IStoreState } from '@client/store'
+import * as React from 'react'
+import { useIntl } from 'react-intl'
+import { useDispatch, useSelector } from 'react-redux'
+import styled from 'styled-components'
+import { HalfWidthInput } from '../../Components'
+
+export enum TabId {
+  COLOUR = 'Colour',
+  IMAGE = 'Image'
+}
+enum BackgroundSize {
+  FILL = 'FILL',
+  TILE = 'TILE'
+}
+const Box = styled.span`
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  border: 2px solid ${({ theme }) => theme.colors.grey300};
+  background-color: ${(p) => (p.color ? p.color : '#fff')};
+`
+const HexInput = styled(HalfWidthInput)`
+  width: 200px;
+`
+export const LoginBackground = () => {
+  const intl = useIntl()
+  const dispatch = useDispatch()
+  const offlineLoginConfiguration = useSelector((store: IStoreState) =>
+    getOfflineData(store)
+  )
+}
